a6480f4eb352e6861287692b0b0b7f2e1fe81b86
Johnnylkc <momozzz1231@gmail.com>
2016-01-08 12:04:06 +0800

    Initial Commit

---
+++ alamofire0108/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alamofire0108
+//
+//  Created by 劉坤昶 on 2016/1/8.
+//  Copyright © 2016年 劉坤昶 Johnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
